898122f8ae9dccba4528cb20586f97e767206f46
Elroi Noronha <elroinoronha2@gmail.com>
2024-07-01 20:18:04 +0530

    testing changed git email address

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello there");
+        System.out.println("second line");
     }
 }
